4689bcb8cbb49c7d3849bb64f2dcc5cd16c19b10
Gustavo <gustgouvea@hotmail.com>
2022-10-24 11:48:37 -0300

    inicio do header, tudo ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Luis Gustavo Gouvêa</h1>
+        <h5 className="text-light">Front-end Developer</h5>
+        <CTA />
+
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/in/luis-gustavo-gouv%C3%AAa-dami%C3%A3o-alves-8a7232224/" target="_blank"></a>
+        <a href="https://github.com/Gustavo121/Gustavo121" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
