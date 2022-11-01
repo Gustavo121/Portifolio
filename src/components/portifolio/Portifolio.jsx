480c70e2040ac650228abcf8f75d53519b533f35
Gustavo <gustgouvea@hotmail.com>
2022-11-01 11:20:26 -0300

    primeiro demo ok, imagem ok

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -1,82 +1,86 @@
 import React from 'react'
-import './portifolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
-const Portifolio = () => {
-  return (
-    <section id='portifolio'>
-      <h5>My Recent Work</h5>
-      <h2>Portifolio</h2>
+// DO NOT USE THE IMAGES IN PRODUCTION
 
-      <div className='container portifolio__container'>
-        <article className='portifolio__item'>
-          <div className="portifolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portifolio item title</h3>
-          <div className='portifolio__item-cta'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>          
-          </div>
-        </article>
-        <article className='portifolio__item'>
-          <div className="portifolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portifolio item title</h3>
-          <div className='portifolio__item-cta'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>          
-          </div>
-        </article>
-        <article className='portifolio__item'>
-          <div className="portifolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portifolio item title</h3>
-          <div className='portifolio__item-cta'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>          
-          </div>
-        </article>
-        <article className='portifolio__item'>
-          <div className="portifolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portifolio item title</h3>
-          <div className='portifolio__item-cta'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>          
-          </div>
-        </article>
-        <article className='portifolio__item'>
-          <div className="portifolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portifolio item title</h3>
-          <div className='portifolio__item-cta'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>          
-          </div>
-        </article>
-        <article className='portifolio__item'>
-          <div className="portifolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portifolio item title</h3>
-          <div className='portifolio__item-cta'>
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>          
-          </div>
-        </article>
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Project developed for food websites',
+    github: 'https://github.com/Gustavo121',
+    demo: 'https://gustavo121.github.io/Project-Food/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Project developed for food websites',
+    github: 'https://github.com/Gustavo121',
+    demo: 'https://gustavo121.github.io/Project-Food/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Project developed for food websites',
+    github: 'https://github.com/Gustavo121',
+    demo: 'https://gustavo121.github.io/Project-Food/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Project developed for food websites',
+    github: 'https://github.com/Gustavo121',
+    demo: 'https://gustavo121.github.io/Project-Food/'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Project developed for food websites',
+    github: 'https://github.com/Gustavo121',
+    demo: 'https://gustavo121.github.io/Project-Food/'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Project developed for food websites',
+    github: 'https://github.com/Gustavo121',
+    demo: 'https://gustavo121.github.io/Project-Food/'
+  },
+
+]
+
+const Portfolio = () => {
+  return (
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>          
+              </div>
+            </article>
+            )
+          })
+        }
       </div>
     </section>
   )
 }
 
-export default Portifolio
+export default Portfolio
