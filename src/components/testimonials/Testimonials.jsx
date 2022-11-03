36f0aa4d04975d809e3b5166cf2b64e467c9b253
Gustavo <gustgouvea@hotmail.com>
2022-11-03 01:05:01 -0300

    testimonials ok. resposivo ok

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
-import './testimonials.css'
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
-  )
-}
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
 
-export default Testimonials
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="Avatar One" />
+          </div>
+          <h5 className="client__name">Enert Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            fugiat expedita iusto corrupti omnis consectetur nemo. Perspiciatis
+            dolores asperiores quaerat vitae blanditiis cupiditate reprehenderit
+            id, eveniet nisi earum pariatur. Illum!
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR2} alt="Avatar Two" />
+          </div>
+          <h5 className="client__name">Enert Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            fugiat expedita iusto corrupti omnis consectetur nemo. Perspiciatis
+            dolores asperiores quaerat vitae blanditiis cupiditate reprehenderit
+            id, eveniet nisi earum pariatur. Illum!
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR3} alt="Avatar Three" />
+          </div>
+          <h5 className="client__name">Enert Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            fugiat expedita iusto corrupti omnis consectetur nemo. Perspiciatis
+            dolores asperiores quaerat vitae blanditiis cupiditate reprehenderit
+            id, eveniet nisi earum pariatur. Illum!
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR4} alt="Avatar Four" />
+          </div>
+          <h5 className="client__name">Enert Achiever</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum
+            fugiat expedita iusto corrupti omnis consectetur nemo. Perspiciatis
+            dolores asperiores quaerat vitae blanditiis cupiditate reprehenderit
+            id, eveniet nisi earum pariatur. Illum!
+          </small>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
