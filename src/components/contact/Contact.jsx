882e65d6380a0334a478c5ec56b092b050845ee6
Gustavo <gustgouvea@hotmail.com>
2022-11-04 12:20:06 -0300

    contact ok, resposivo ok

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,8 +3,20 @@ import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
 import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_hvy8cde', 'template_1tpv84i', form.current, '7aEjIDSWsXAdM4ZZX')
+
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get in Touch</h5>
@@ -33,7 +45,7 @@ const Contact = () => {
         </div>
         
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your Full Name' required />
           <input type="email" name="email" placeholder='Your Email' required/>
           <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
